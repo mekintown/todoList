d6be69789697881be6d3b8b5e95bd50a6f7fa7e3
Maekin <74455990+mekintown@users.noreply.github.com>
2023-06-29 15:34:53 +0700

    Add render project function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 import createView from "./views/homePageView";
 import runTaskScript from "./controllers/taskController";
+import runProjectScript from "./controllers/projectController";
 
+runProjectScript();
 runTaskScript();
 createView();
